4061585695e8fb09d8fa27069c25bcde014ff116
GianniLeone <55284172+GianniLeone@users.noreply.github.com>
2022-12-17 13:46:59 -0700

    Added character flip

--- Edge_of_the_World/Assets/PlayerMovement.cs
+++ Edge_of_the_World/Assets/PlayerMovement.cs
@@ -21,7 +21,27 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Get user input
         moveDirection = Input.GetAxis("Horizontal");
+
+        // Animate
+        if (moveDirection > 0 && !facingRight)
+        {
+            flipCharacter();
+        }
+        else if (moveDirection < 0 && facingRight)
+        {
+            flipCharacter();
+        }
+
+        // Move player
         rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
     }
+
+    private void flipCharacter()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
+
 }
